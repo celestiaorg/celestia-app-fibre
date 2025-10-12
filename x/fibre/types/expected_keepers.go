b495420613a916454e8fddb9f77257ff32fb2a4e
Rootul Patel <rootulp@gmail.com>
2025-10-12 10:20:25 -0400

    docs: remove comment

--- x/fibre/types/expected_keepers.go
+++ x/fibre/types/expected_keepers.go
@@ -4,7 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// BankKeeper defines the contract needed to be fulfilled for banking dependencies.
 type BankKeeper interface {
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
