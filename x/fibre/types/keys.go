dbf49c9144c032cc70f2751ace8bf5554b1fc0da
Rootul P <rootulp@gmail.com>
2025-10-22 14:33:16 -0400

    feat(x/fibre): initial keeper implementation (#4)
    Part of https://github.com/celestiaorg/celestia-app-fibre/issues/1
    
    Adds basic CRUD operations to the keeper for:
    - Escrow accounts (Get, Set, Delete)
    - Withdrawals (Get, Set, Delete) and GetBySigner
    - ProcessedPayment (Get, Set, Delete) and IsPaymentProcessed
    
    We may need more later 🤷‍♂️ but this seems safe to merge as-is

---
+++ x/fibre/types/keys.go
@@ -0,0 +1,55 @@
+package types
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "fibre"
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+	// RouterKey defines the module's message routing key
+	RouterKey = ModuleName
+	// ParamsKey defines the key used for storing module parameters
+	ParamsKey = "params"
+)
+
+// Store key prefixes
+var (
+	// EscrowAccountKeyPrefix is the prefix for escrow account keys
+	EscrowAccountKeyPrefix = []byte{0x01}
+	// WithdrawalKeyPrefix is the prefix for withdrawal keys
+	WithdrawalKeyPrefix = []byte{0x02}
+	// PaymentPromiseKeyPrefix is the prefix for processed payment promise keys
+	PaymentPromiseKeyPrefix = []byte{0x03}
+	// ParamsKeyPrefix is the prefix for params
+	ParamsKeyPrefix = []byte{0x04}
+)
+
+// EscrowAccountKey returns the store key for an escrow account
+func EscrowAccountKey(signer string) []byte {
+	return append(EscrowAccountKeyPrefix, []byte(signer)...)
+}
+
+// WithdrawalKey returns the store key for a withdrawal
+func WithdrawalKey(signer string, requestedTimestamp time.Time) []byte {
+	key := WithdrawalsBySignerPrefix(signer)
+	key = append(key, []byte("/")...)
+	timestampBytes := sdk.FormatTimeBytes(requestedTimestamp)
+	return append(key, timestampBytes...)
+}
+
+// WithdrawalsBySignerPrefix returns the prefix for all withdrawals by a signer
+func WithdrawalsBySignerPrefix(signer string) []byte {
+	return append(WithdrawalKeyPrefix, []byte(signer)...)
+}
+
+// PaymentPromiseKey returns the store key for a payment promise. Note: all
+// payment promises that are stored in the SDK module state have already been
+// processed.
+func PaymentPromiseKey(payment_promise_hash []byte) []byte {
+	return append(PaymentPromiseKeyPrefix, payment_promise_hash...)
+}
