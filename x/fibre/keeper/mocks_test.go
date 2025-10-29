458dc1b30e5f341cd3939a549c2a952fd5a010e4
Rootul P <rootulp@gmail.com>
2025-10-29 09:37:26 -0400

    feat(x/fibre): implement CLI (#35)
    Prerequisite: https://github.com/celestiaorg/fibre-da-spec/pull/28
    
    Part of https://github.com/celestiaorg/celestia-app-fibre/issues/1
    
    ## Testing
    
    Query 
    
    ```
    $ celestia-appd query fibre --help
    Querying commands for the fibre module
    
    Usage:
      celestia-appd query fibre [flags]
      celestia-appd query fibre [command]
    
    Available Commands:
      escrow-account       Query an escrow account by signer address
      is-payment-processed Query whether a payment promise has been processed
      params               Query the current fibre parameters
      withdrawals          Query all withdrawals for an escrow account by signer address
    
    $ celestia-appd query fibre params
    params:
      gas_per_blob_byte: 1
      payment_promise_retention_window: 86400s
      payment_promise_timeout: 3600s
      withdrawal_delay: 86400s
    ```
    
    Tx
    
    ```
    $ celestia-appd tx fibre --help
    fibre transactions subcommands
    
    Usage:
      celestia-appd tx fibre [flags]
      celestia-appd tx fibre [command]
    
    Available Commands:
      deposit-to-escrow       Deposit tokens to an escrow account
      pay-for-fibre           Process a payment promise with validator signatures
      payment-promise-timeout Process a timed-out payment promise
      request-withdrawal      Request withdrawal from an escrow account
    ```

--- x/fibre/keeper/mocks_test.go
+++ x/fibre/keeper/mocks_test.go
@@ -1,30 +1,27 @@
 package keeper_test
 
 import (
+	"context"
+
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // MockBankKeeper implements the expected BankKeeper interface for testing
 type MockBankKeeper struct {
-	SendCoinsFromAccountToModuleFn func(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromAccountToModuleFn func(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
 
-func (m *MockBankKeeper) SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
+func (m *MockBankKeeper) SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
 	if m.SendCoinsFromAccountToModuleFn != nil {
 		return m.SendCoinsFromAccountToModuleFn(ctx, senderAddr, recipientModule, amt)
 	}
 	return nil
 }
 
-func (m *MockBankKeeper) SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error {
+func (m *MockBankKeeper) SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error {
 	return nil
-}
-
-func (m *MockBankKeeper) GetModuleAddress(moduleName string) sdk.AccAddress {
-	return authtypes.NewModuleAddress(moduleName)
 }
 
 // MockStakingKeeper implements the expected StakingKeeper interface for testing
@@ -33,7 +30,7 @@ type MockStakingKeeper struct {
 	validatorKeys  map[int64]ed25519.PrivKey
 }
 
-func (m *MockStakingKeeper) GetHistoricalInfo(ctx sdk.Context, height int64) (stakingtypes.HistoricalInfo, error) {
+func (m *MockStakingKeeper) GetHistoricalInfo(ctx context.Context, height int64) (stakingtypes.HistoricalInfo, error) {
 	if m.historicalInfo != nil {
 		if info, ok := m.historicalInfo[height]; ok {
 			return info, nil
