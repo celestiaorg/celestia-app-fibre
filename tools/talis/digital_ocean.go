e2db2cae661f4ce8e1960e3e0b02432c5e1b9b8f
evan-forbes <evan.samuel.forbes@gmail.com>
2025-11-09 11:56:00 -0600

    chore: cleanup

--- tools/talis/digital_ocean.go
+++ tools/talis/digital_ocean.go
@@ -440,24 +440,6 @@ func hasAllTags(candidate, want []string) bool {
 	return true
 }
 
-// checkForRunningExperiments checks if there are any talis instances running.
-// It returns a list of all talis droplets.
-func checkForRunningExperiments(ctx context.Context, client *godo.Client) ([]godo.Droplet, error) {
-	droplets, err := listAllDroplets(ctx, client)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list droplets: %w", err)
-	}
-
-	var talisDroplets []godo.Droplet
-	for _, d := range droplets {
-		if slices.Contains(d.Tags, "talis") {
-			talisDroplets = append(talisDroplets, d)
-		}
-	}
-
-	return talisDroplets, nil
-}
-
 func checkForRunningDOExperiments(ctx context.Context, client *godo.Client, experimentID, chainID string) (bool, error) {
 	if client == nil {
 		return false, nil
