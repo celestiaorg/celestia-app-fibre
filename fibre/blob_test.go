1e475d24c70f151dfb46ccbad3cad8aa046e627b
Wondertan <hlibwondertan@gmail.com>
2025-12-04 15:07:45 +0400

    clean up blob tests

--- fibre/blob_test.go
+++ fibre/blob_test.go
@@ -73,84 +73,7 @@ func TestBlobHeaderV0_EncodeToRows_DecodeFromRows(t *testing.T) {
 	}
 }
 
-func TestBlob_EncodeDecode(t *testing.T) {
-	testData := []byte("test data for encoding")
-
-	cfg := BlobConfig{
-		OriginalRows:  8,
-		ParityRows:    24,
-		CodingWorkers: 2,
-		RowSizeMin:    64,
-		MaxBlobSize:   1024,
-	}
-
-	blob, err := NewBlob(testData, cfg)
-	require.NoError(t, err)
-	require.NotNil(t, blob)
-
-	commitment := blob.Commitment()
-
-	reconstructBlob := NewEmptyBlob(cfg, commitment)
-
-	totalRows := cfg.OriginalRows + cfg.ParityRows
-	for i := 0; i < totalRows; i++ {
-		row, err := blob.Row(i)
-		require.NoError(t, err)
-
-		isDone, err := reconstructBlob.SetRow(row)
-		require.NoError(t, err)
-		if isDone {
-			break
-		}
-	}
-
-	err = reconstructBlob.Reconstruct()
-	require.NoError(t, err)
-
-	reconstructedData := reconstructBlob.Data()
-	require.Equal(t, testData, reconstructedData)
-}
-
-func TestBlob_NotEnoughRows(t *testing.T) {
-	cfg := BlobConfig{
-		OriginalRows:  8,
-		ParityRows:    24,
-		CodingWorkers: 2,
-		RowSizeMin:    64,
-		MaxBlobSize:   1024,
-	}
-
-	blob, err := NewBlob([]byte("test data"), cfg)
-	require.NoError(t, err)
-
-	reconstructBlob := NewEmptyBlob(cfg, blob.Commitment())
-
-	for i := 0; i < 3; i++ {
-		row, err := blob.Row(i)
-		require.NoError(t, err)
-		_, err = reconstructBlob.SetRow(row)
-		require.NoError(t, err)
-	}
-
-	err = reconstructBlob.Reconstruct()
-	require.ErrorIs(t, err, ErrNotEnoughRows)
-}
-
-func TestBlob_NotFound(t *testing.T) {
-	cfg := BlobConfig{
-		OriginalRows:  8,
-		ParityRows:    24,
-		CodingWorkers: 2,
-		RowSizeMin:    64,
-		MaxBlobSize:   1024,
-	}
-
-	blob := NewEmptyBlob(cfg, Commitment{})
-	err := blob.Reconstruct()
-	require.ErrorIs(t, err, ErrBlobNotFound)
-}
-
-func TestBlob_ErasureCoding(t *testing.T) {
+func TestBlob_Reconstruct(t *testing.T) {
 	testData := []byte("test erasure coding reconstruction")
 
 	cfg := BlobConfig{
@@ -164,8 +87,6 @@ func TestBlob_ErasureCoding(t *testing.T) {
 	blob, err := NewBlob(testData, cfg)
 	require.NoError(t, err)
 
-	commitment := blob.Commitment()
-
 	totalRows := cfg.OriginalRows + cfg.ParityRows
 	allRows := make([]*rsema1d.RowInclusionProof, totalRows)
 	for i := 0; i < totalRows; i++ {
@@ -175,7 +96,7 @@ func TestBlob_ErasureCoding(t *testing.T) {
 	}
 
 	testReconstruct := func(t *testing.T, rows []*rsema1d.RowInclusionProof) {
-		reconstructBlob := NewEmptyBlob(cfg, commitment)
+		reconstructBlob := NewEmptyBlob(cfg, blob.Commitment())
 
 		for _, row := range rows {
 			_, err = reconstructBlob.SetRow(row)
@@ -206,5 +127,21 @@ func TestBlob_ErasureCoding(t *testing.T) {
 			}
 		}
 		testReconstruct(t, mixedRows)
+	})
+
+	t.Run("NotEnoughRows", func(t *testing.T) {
+		reconstructBlob := NewEmptyBlob(cfg, blob.Commitment())
+		for i := 0; i < cfg.OriginalRows-1; i++ {
+			_, err = reconstructBlob.SetRow(allRows[i])
+			require.NoError(t, err)
+		}
+		err = reconstructBlob.Reconstruct()
+		require.ErrorIs(t, err, ErrNotEnoughRows)
+	})
+
+	t.Run("NotFound", func(t *testing.T) {
+		emptyBlob := NewEmptyBlob(cfg, blob.Commitment())
+		err := emptyBlob.Reconstruct()
+		require.ErrorIs(t, err, ErrBlobNotFound)
 	})
 }
